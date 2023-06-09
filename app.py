006d42af211995f5f8871f39712098485cc22f9b
Yoji975 <136058733+Yoji975@users.noreply.github.com>
2023-06-09 18:21:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("ダイエットを助けるチャットボット")
+st.write("ダイエットに関して、何にお困りですか？")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
