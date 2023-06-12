c63f18289aa7ff884e6f1de32a2c3e00bc7fa946
Yoji975 <136058733+Yoji975@users.noreply.github.com>
2023-06-12 15:52:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,7 @@ with col2:
     messages = st.session_state["messages"]
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
-        speaker = "🙂"
-        if message["role"]=="assistant":
-            speaker="🤖"
-
-        st.write(speaker + ": " + message["content"])
+        st.write(message["content"])
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
