21734339947059004e5b21446b701845d3b560de
Yoji975 <136058733+Yoji975@users.noreply.github.com>
2023-06-13 09:54:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ with col2:
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
           str=str+message["content"]+"<br>"
           cnt=cnt+1
+          engine.say("Hello, World")
+          engine.runAndWait()
           if cnt==2:
             str=str+"========="+"<br>"
             cnt=0
