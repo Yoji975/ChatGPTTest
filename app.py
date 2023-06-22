1449a0c488655a3c29a8af3c7d36567db96a032c
Yoji975 <136058733+Yoji975@users.noreply.github.com>
2023-06-22 17:19:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ with col2:
     #st.audio(sound_file)
 
     #HOYAAPIにアクセス
-    url = "https://scapi-eu.readspeaker.com/a/speak?key=b6ddbe58ee4dae1f3987cb9f811f112f&lang=en_us&voice=Lizzy&text="+result
+    url = "https://scapi-eu.readspeaker.com/a/speak?key=b6ddbe58ee4dae1f3987cb9f811f112f&lang=en_us&voice=Julie-DNN&text="+result
     r = requests.get(url)
     response = r.content
     
